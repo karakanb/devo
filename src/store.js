845222557ebbb463f1f24527e827e8347b2822ff
Burak Karakan <burak.karakan@gmail.com>
2018-07-25 17:22:20 +0300

    Added vuex store actions.

--- src/store.js
+++ src/store.js
@@ -1,16 +1,45 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    github: localStorage.getItem('github'),
+    hackernews: localStorage.getItem('hackernews'),
+    updated_at: {
+      github: '',
+      hackernews: '',
+      threshold: 60 * 60 * 1000,
+    },
   },
   mutations: {
-
+    setGithubData(state, data) {
+      state.github = data;
+      state.updated_at.github = new Date();
+      localStorage.setItem('github', data);
+    },
+    setHackernewsData(state, data) {
+      state.hackernews = data;
+      state.updated_at.hackernews = new Date();
+      localStorage.setItem('hackernews', data);
+    },
   },
   actions: {
-
+    async updateHackernews(context) {
+      const { threshold } = context.state.updated_at;
+      if ((new Date()) - context.state.updated_at.hackernews > threshold) {
+        const response = await axios.get('https://hn-api.now.sh/');
+        context.commit('setHackernewsData', response.data);
+      }
+    },
+    async updateGithub(context) {
+      const { threshold } = context.state.updated_at;
+      if ((new Date()) - context.state.updated_at.hackernews > threshold) {
+        const response = await axios.get('https://gh-api.now.sh/');
+        context.commit('setGithubData', response.data);
+      }
+    },
   },
 });
