34958682ee3af7f0bb5d27de823afbe955cec5fd
Burak Karakan <burak.karakan@gmail.com>
2019-09-30 01:02:18 +0200

    Linter complained.

--- src/main.js
+++ src/main.js
@@ -2,13 +2,17 @@ import Vue from 'vue';
 import 'flexboxgrid';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faGithub, faHackerNewsSquare, faProductHunt, faDev } from '@fortawesome/free-brands-svg-icons';
-import { faSyncAlt, faCodeBranch, faStar, faChevronUp, faComment, faExternalLinkAlt, faSun, faMoon, faCheck, faCaretDown, faNewspaper, faHeart } from '@fortawesome/free-solid-svg-icons';
+import {
+  faSyncAlt, faCodeBranch, faStar, faChevronUp, faComment, faExternalLinkAlt, faSun, faMoon,
+  faCheck, faCaretDown, faNewspaper, faHeart,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import store from './store';
 
 library.add([faGithub, faHackerNewsSquare, faProductHunt, faSyncAlt,
-  faCodeBranch, faStar, faChevronUp, faComment, faExternalLinkAlt, faSun, faMoon, faCheck, faCaretDown, faNewspaper, faDev, faHeart]);
+  faCodeBranch, faStar, faChevronUp, faComment, faExternalLinkAlt, faSun, faMoon, faCheck, faCaretDown,
+  faNewspaper, faDev, faHeart]);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
