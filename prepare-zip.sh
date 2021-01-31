e3798e58f466097f2aec8018a741ee56e613ab0a
Burak Karakan <burak.karakan@gmail.com>
2021-01-31 18:34:10 +0100

    Automate the creation of ZIP files for store uploads

---
+++ prepare-zip.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+mkdir dist-zip
+
+# Prepare the dist ZIP to be uploaded
+zip -r -9 -X dist-zip/dist.zip dist
+
+# Prepare the source code ZIP
+zip -r -9 -X dist-zip/src.zip . -x "*dist/*" -x "*.git/*" -x "*.DS_Store" -x "*node_modules*" -x "*.github/*" -x "*docs/*"
