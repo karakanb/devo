c91b97f520687290a8f9ef328bbf7ce701cc69fe
Burak Karakan <burak.karakan@gmail.com>
2021-08-03 19:31:52 +0100

    create a new script to upload the extension to the Chrome store

---
+++ scripts/deploy-chrome.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+set -e
+
+# check that the env variables are set
+declare -a vars=(SOURCE_ZIP EXTENSION_ID CLIENT_ID CLIENT_SECRET REFRESH_TOKEN)
+for var_name in "${vars[@]}"
+do
+  if [ -z "$(eval "echo \$$var_name")" ]; then
+    echo "Missing environment variable $var_name"
+    exit 1
+  fi
+done
+
+# install the chrome uploader tool
+npm install --silent --no-progress -g chrome-webstore-upload-cli
+
+# upload and publish the extension
+webstore upload \
+    --source $SOURCE_ZIP \
+    --extension-id $EXTENSION_ID \
+    --client-id $CLIENT_ID \
+    --client-secret $CLIENT_SECRET \
+    --refresh-token $REFRESH_TOKEN
+    --auto-publish
+
+echo "Successfully published to chrome store"
--- scripts/prepare-zip.sh
+++ scripts/prepare-zip.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+set -e
 
 mkdir dist-zip
 
