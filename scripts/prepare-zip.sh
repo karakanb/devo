044e24317d4dbc79bf790d2b4fdadb59f269f8f0
Burak Karakan <burak.karakan@gmail.com>
2021-08-03 19:49:10 +0100

    recreate the zips at every invocation of the build command

--- scripts/prepare-zip.sh
+++ scripts/prepare-zip.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 set -e
 
-mkdir dist-zip
+rm -rf dist-zip
+mkdir -p dist-zip
 
 # Prepare the dist ZIP to be uploaded
-zip -r -9 -X dist-zip/dist.zip dist
+zip -r -9 -X -qq dist-zip/dist.zip dist
 
 # Prepare the source code ZIP
-zip -r -9 -X dist-zip/src.zip . -x "*dist/*" -x "*.git/*" -x "*.DS_Store" -x "*node_modules*" -x "*.github/*" -x "*docs/*"
+zip -r -9 -X -qq dist-zip/src.zip . -x "*dist/*" -x "*.git/*" -x "*.DS_Store" -x "*node_modules*" -x "*.github/*" -x "*docs/*"
