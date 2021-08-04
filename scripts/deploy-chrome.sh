d72dbe66245b9192968aabd1fd6e304e2b0d7011
Burak Karakan <burak.karakan@gmail.com>
2021-08-04 13:39:35 +0100

    fix the deploy script for chrome

--- scripts/deploy-chrome.sh
+++ scripts/deploy-chrome.sh
@@ -20,7 +20,7 @@ webstore upload \
     --extension-id $EXTENSION_ID \
     --client-id $CLIENT_ID \
     --client-secret $CLIENT_SECRET \
-    --refresh-token $REFRESH_TOKEN
+    --refresh-token $REFRESH_TOKEN \
     --auto-publish
 
 echo "Successfully published to chrome store"
