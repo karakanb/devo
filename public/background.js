52a85acff88a2b99eece5cf185f9627dde84be0b
Aayush Lamichhane <lamichhaneaayush5@gmail.com>
2025-06-02 15:23:18 +0545

    fix errors on manifest v3

--- public/background.js
+++ public/background.js
@@ -1,12 +1,13 @@
 chrome.runtime.onInstalled.addListener((details) => {
 
   // remove the legacy state.
-  localStorage.removeItem('vuex');
+  chrome.storage.local.remove('vuex', () => { });
 
   // remove the state for the previous version.
   const manifestVersion = chrome.runtime.getManifest().version;
   if (details.previousVersion && details.previousVersion !== manifestVersion) {
-    localStorage.removeItem(`vuex-state-${details.previousVersion}`);
-    console.log(`Successfully removed the old state for ${details.previousVersion}`);
+    chrome.storage.local.remove(`vuex-state-${details.previousVersion}`, () => {
+      console.log(`Successfully removed the old state for ${details.previousVersion}`);
+    });
   }
 });
